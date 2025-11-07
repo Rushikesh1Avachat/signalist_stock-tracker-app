5bfd6c1e6aca1e32ee266e09085041a533757a1f
Rushikesh1 Avachat <avachatrushikesh45@gmail.com>
2025-11-07 23:08:37 +0530

    add runtime node

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,9 +1,10 @@
 import {serve} from "inngest/next";
 import {inngest} from "@/lib/inngest/client";
 import {sendDailyNewsSummary, sendSignUpEmail} from "@/lib/inngest/functions";
+// ✅ Force Node runtime (important for Inngest)
+export const runtime = "nodejs";
 
 
-// @ts-ignore
 export const { GET, POST, PUT } = serve({
     client: inngest,
     functions: [sendSignUpEmail , sendDailyNewsSummary],
